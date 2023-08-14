4f12f4f8d4ee21585c771ff349d4a98727c6dc8a
Archon <archongum@outlook.com>
2023-08-14 10:04:35 +0000

    Duplicate from Archon/omymodels-online

---
+++ Home.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from omymodels import create_models
+
+
+DDL_EXAMPLE = r"""
+CREATE TABLE "material" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar NOT NULL,
+  "description" text,
+  "link" varchar NOT NULL,
+  "type" integer NOT NULL,
+  "additional_properties" json,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp
+);
+""".strip()
+
+st.set_page_config(page_title="omymodels Online", page_icon="🌍", layout="wide")
+
+st.markdown(
+    r"""
+# omymodels Online
+> 💪Powered by: https://github.com/xnuinside/omymodels
+""".strip()
+)
+
+
+c_selectbox_models_type = st.selectbox(
+    "Model Type",
+    options=["gino", "sqlalchemy", "sqlalchemy_core", "pydantic", "dataclass"],
+    index=1,
+)
+
+c_text_ddl, c_code_result = st.columns([5, 6])
+
+ddl = c_text_ddl.text_area("DDL", value=DDL_EXAMPLE, height=473)
+
+# ddl: Optional[str] = (None,)
+# ddl_path: Optional[str] = (None,)
+# dump: bool = (True,)
+# dump_path: str = ("models.py",)
+# singular: bool = (False,)
+# naming_exceptions: Optional[List] = (None,)
+# models_type: str = ("gino",)
+# schema_global: Optional[bool] = (True,)
+# defaults_off: Optional[bool] = (False,)
+# exit_silent: Optional[bool] = (False,)
+# no_auto_snake_case: Optional[bool] = False
+
+result = create_models(
+    ddl,
+    models_type=c_selectbox_models_type,
+    dump=False,
+)
+
+c_code_result.code(result["code"], language="python", line_numbers=True)
---
+++ README.md
@@ -0,0 +1,28 @@
+---
+title: omymodels-online
+emoji: 🌍
+colorFrom: green
+colorTo: indigo
+python_version: 3.1
+sdk: streamlit
+sdk_version: 1.24.0
+app_file: Home.py
+app_port: 8051
+pinned: true
+duplicated_from: Archon/omymodels-online
+---
+
+# omymodels-online
+
+## Installation
+
+```sh
+python -m venv .venv
+. .venv/bin/activate
+
+pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
+
+streamlit run Home.py
+```
+
+打开浏览器，访问: http://127.0.0.1:8051
